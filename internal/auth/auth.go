40a7afc58397792741fd54ba85e417cd8820fc0c
StellarisJAY <760409558@qq.com>
2022-07-26 12:22:57 +0800

    feat: 设备授权服务

---
+++ internal/auth/auth.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"github.com/stellarisJAY/goim/internal/auth/service"
+	"github.com/stellarisJAY/goim/pkg/naming"
+	"github.com/stellarisJAY/goim/pkg/proto/pb"
+	"google.golang.org/grpc"
+	"net"
+)
+
+var server *grpc.Server
+
+func Init() {
+	server = grpc.NewServer()
+	// 注册授权服务
+	err := naming.RegisterService(naming.ServiceRegistration{
+		ServiceName: "auth",
+		Address:     "127.0.0.1:9999",
+	})
+	if err != nil {
+		panic(err)
+	}
+	pb.RegisterAuthServer(server, &service.AuthServiceImpl{})
+}
+
+func Start() error {
+	listen, err := net.Listen("tcp", ":9999")
+	if err != nil {
+		return err
+	}
+	return server.Serve(listen)
+}
---
+++ internal/auth/service/auth.go
@@ -0,0 +1,123 @@
+package service
+
+import (
+	context "context"
+	"errors"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/stellarisJAY/goim/pkg/proto/pb"
+	"log"
+	"time"
+)
+
+const (
+	TokenTimeout = time.Hour * 2
+)
+
+var secretKey = []byte("secret key not defined yet")
+
+type Claims struct {
+	jwt.StandardClaims
+	UserId   string `json:"userId"`
+	DeviceId string `json:"deviceId"`
+}
+
+// AuthServiceImpl 授权服务实现
+type AuthServiceImpl struct {
+}
+
+// AuthorizeDevice 为用户设备授权，检查请求的密码，最终生成一个与用户ID和设备ID绑定的Token
+func (as *AuthServiceImpl) AuthorizeDevice(ctx context.Context, request *pb.AuthRequest) (*pb.AuthResponse, error) {
+	// 检查用户ID 和 密码
+	if !verifyPassword(request.UserId, request.Password) {
+		return &pb.AuthResponse{
+			Code: pb.WrongPassword,
+		}, nil
+	}
+	log.Println("authorize: ", request)
+	// 生成 Token
+	if token, err := generateToken(request.UserId, request.DeviceId); err != nil {
+		return &pb.AuthResponse{
+			Code:  pb.Error,
+			Token: "",
+		}, nil
+	} else {
+		return &pb.AuthResponse{
+			Code:  pb.Success,
+			Token: token,
+		}, nil
+	}
+}
+
+// LoginDevice 设备登录，该方法在设备接入聊天服务前调用，用于检查Token是否合法，并最终记录session
+func (as *AuthServiceImpl) LoginDevice(ctx context.Context, request *pb.LoginRequest) (*pb.LoginResponse, error) {
+	// 解析Token
+	claims, err := parseToken(request.Token)
+	if err != nil {
+		log.Println(err)
+		return &pb.LoginResponse{
+			Code: pb.AccessDenied,
+		}, nil
+	}
+	log.Println(claims)
+	// 检查Token是否过期
+	if time.Now().After(time.UnixMilli(claims.ExpiresAt)) {
+		log.Println("token expired: ", claims.ExpiresAt)
+		return &pb.LoginResponse{
+			Code: pb.AccessDenied,
+		}, nil
+	}
+	if claims.DeviceId != request.DeviceId || claims.UserId != request.UserId {
+		log.Println(claims.DeviceId, ", ", claims.UserId)
+		return &pb.LoginResponse{
+			Code: pb.AccessDenied,
+		}, nil
+	}
+	// 缓存设备的session信息，在 Redis中记录 userId: {deviceId: {gateway, channel}}
+	if err := newSession(request.Gateway, request.Channel, claims.UserId, claims.DeviceId); err != nil {
+		return &pb.LoginResponse{
+			Code:    pb.Error,
+			Message: err.Error(),
+		}, nil
+	}
+	return &pb.LoginResponse{
+		Code: pb.Success,
+	}, nil
+}
+
+// generateToken 通过用户ID 和 设备ID 生成Token
+func generateToken(userId, deviceId string) (string, error) {
+	expireTime := time.Now().Add(TokenTimeout).UnixMilli()
+	claims := Claims{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expireTime,
+		},
+		UserId:   userId,
+		DeviceId: deviceId,
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(secretKey)
+}
+
+// parseToken 解析Token
+func parseToken(signed string) (*Claims, error) {
+	claims := new(Claims)
+	c, err := jwt.ParseWithClaims(signed, claims, func(token *jwt.Token) (interface{}, error) {
+		return secretKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if c.Valid {
+		return claims, nil
+	}
+	return nil, errors.New("invalid token")
+}
+
+func verifyPassword(userId, password string) bool {
+	return true
+}
+
+// newSession 在 session管理器中记录 用户设备ID 与 网关服务器的绑定关系
+func newSession(gateway, channel, userId, deviceId string) error {
+	return nil
+}
