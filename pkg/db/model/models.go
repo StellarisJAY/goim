8c06c6faddd70891b496499a7d5bcf64a2c7eabd
StellarisJAY <760409558@qq.com>
2022-07-26 16:20:31 +0800

    feat: User和设备登录记录数据库模型

---
+++ pkg/db/model/models.go
@@ -0,0 +1,20 @@
+package model
+
+import "time"
+
+// User 用户表
+type User struct {
+	ID        int64     `gorm:"column:id;primary key;"`
+	Account   string    `gorm:"column:account"`
+	Password  string    `gorm:"column:password"`
+	NickName  string    `gorm:"column:nick_name"`
+	CreatedAt time.Time `gorm:"column:create_time"`
+}
+
+// DeviceLogin 设备登录记录表
+type DeviceLogin struct {
+	UserID    int64  `gorm:"column:user_id"`
+	DeviceID  string `gorm:"column:device_id"`
+	Timestamp int64  `gorm:"column:timestamp"`
+	Ip        string `gorm:"column:ip"`
+}
