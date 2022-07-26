7aa3c042aa61b56dd0498ab58c5e939c8b331622
StellarisJAY <760409558@qq.com>
2022-07-26 12:22:14 +0800

    feat: 网关服务的消息中转RPC实现

---
+++ internal/gateway/relay.go
@@ -0,0 +1,67 @@
+package gateway
+
+import (
+	"context"
+	"github.com/stellarisJAY/goim/pkg/proto/pb"
+	"github.com/stellarisJAY/goim/pkg/websocket"
+	"google.golang.org/protobuf/proto"
+	"log"
+)
+
+// PushMessage 消息下行 RPC 服务
+func (s *Server) PushMessage(ctx context.Context, request *pb.PushMsgRequest) (*pb.PushMsgResponse, error) {
+	// 获取目标 channel
+	load, ok := s.wsServer.Channels.Load(request.Channel)
+	resp := new(pb.PushMsgResponse)
+	if ok {
+		// 将 message 编码后向channel发送
+		channel := load.(*websocket.Channel)
+		marshal, err := proto.Marshal(request.Message)
+		if err != nil {
+			return nil, err
+		}
+		err = channel.Push(marshal)
+		if err != nil {
+			return nil, err
+		}
+		resp.Base.Code = pb.Success
+		return resp, nil
+	} else {
+		resp.Base.Code = pb.NotFound
+		resp.Base.Message = "channel not found"
+		return resp, nil
+	}
+}
+
+// Broadcast 消息广播下行，向多个channel发送相同的消息
+func (s *Server) Broadcast(ctx context.Context, request *pb.BroadcastRequest) (*pb.BroadcastResponse, error) {
+	channelIds := request.Channels
+	channels := make([]*websocket.Channel, 0, len(channelIds))
+	fails := make([]string, 0, len(channelIds))
+	// 获取channel对象，如果channel不存在或不可用则将ID加入到失败列表
+	for _, id := range channelIds {
+		if c, ok := s.wsServer.Channels.Load(id); ok {
+			channel := c.(*websocket.Channel)
+			if channel.Available() {
+				channels = append(channels, channel)
+				continue
+			}
+		}
+		fails = append(fails, id)
+	}
+	// 编码消息
+	marshal, err := proto.Marshal(request.Message)
+	if err != nil {
+		return nil, err
+	}
+	// 向每个channel推送消息
+	for _, channel := range channels {
+		err := channel.Push(marshal)
+		if err != nil {
+			// 推送失败，将channelID添加到失败列表
+			log.Println("failed to push message to channel: ", channel.ID())
+			fails = append(fails, channel.ID())
+		}
+	}
+	return &pb.BroadcastResponse{Fails: fails}, nil
+}
