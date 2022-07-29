ace319232ddf7e24348b842b78c3e052666b5946
StellarisJAY <760409558@qq.com>
2022-07-29 11:50:30 +0800

    feat: 离线消息持久化处理

--- internal/transfer/handler/message_handler.go
+++ internal/transfer/handler/message_handler.go
@@ -1,18 +1,62 @@
 package handler
 
 import (
+	"fmt"
 	"github.com/Shopify/sarama"
+	"github.com/stellarisJAY/goim/pkg/db/dao"
+	"github.com/stellarisJAY/goim/pkg/db/model"
 	"github.com/stellarisJAY/goim/pkg/proto/pb"
 	"google.golang.org/protobuf/proto"
 	"log"
 )
 
+// MessageTransferHandler 消息中转处理器
+// 1. 从消息队列消费到一条消息后，
 var MessageTransferHandler = func(message *sarama.ConsumerMessage) error {
 	value := message.Value
 	msg := new(pb.BaseMsg)
 	if err := proto.Unmarshal(value, msg); err != nil {
 		return err
 	}
-	log.Printf("transfer received message, from: %d, to: %d, content: %s, flag: %d", msg.From, msg.To, msg.Content, msg.Flag)
+	var err error
+	switch byte(msg.Flag) {
+	case pb.MessageFlagFrom:
+		err = handleSingleMessage(msg)
+	case pb.MessageFlagGroup:
+		handleGroupChat(msg)
+	default:
+	}
+	if err != nil {
+		log.Println(err)
+	}
 	return nil
+}
+
+// handleSingleMessage 单聊消息处理
+// 1. 在Redis自增用户的收件序列号，然后用自增的结果作为当前消息的序号
+// 2. 获取到序列号后，将消息写入MongoDB作为离线消息保存。
+// 3. Mongo写入完成后，尝试向目标用户所在的网关发送RPC推送消息
+func handleSingleMessage(message *pb.BaseMsg) error {
+	seq, err := dao.IncrUserSeq(message.To)
+	if err != nil {
+		return fmt.Errorf("increment user sequence number error %w", err)
+	}
+	offlineMessage := &model.OfflineMessage{
+		From:      message.From,
+		To:        message.To,
+		Content:   []byte(message.Content),
+		Timestamp: message.Timestamp,
+		Seq:       seq,
+		Flag:      pb.MessageFlagFrom,
+	}
+	err = dao.InsertOfflineMessage(offlineMessage)
+	if err != nil {
+		return fmt.Errorf("insert offline message error %w", err)
+	}
+	return nil
+}
+
+// handleGroupChat 群聊消息处理
+func handleGroupChat(message *pb.BaseMsg) {
+
 }
