bd656570a6330798bf6602fcbaac1ad449f39979
StellarisJAY <760409558@qq.com>
2022-08-03 19:10:33 +0800

    feat: 群邀请表和好友申请表创建

--- pkg/db/mongo.go
+++ pkg/db/mongo.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"github.com/stellarisJAY/goim/pkg/config"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"time"
@@ -12,6 +13,7 @@ const (
 	MongoDBName               = "db_goim"
 	CollectionOfflineMessage  = "offlineMessage"
 	CollectionGroupInvitation = "groupInvitation"
+	CollectionFriendRequest   = "friendRequest"
 )
 
 var Day = int64(time.Hour) * 24
@@ -33,13 +35,31 @@ func InitMongoDB() (*mongo.Client, error) {
 	if err != nil {
 		panic(err)
 	}
-	offlineMessageExpire := Day * int64(config.Config.Message.OfflineExpireTime)
-	_ = client.
-		Database(MongoDBName).
-		CreateCollection(context.TODO(), CollectionOfflineMessage,
-			&options.CreateCollectionOptions{ExpireAfterSeconds: &offlineMessageExpire})
-	groupInvitationExpire := Day * 3
+
+	// 创建离线消息缓存表，离线消息只保留7天
+	offlineMessageOptions := options.CreateCollection().SetExpireAfterSeconds(Day * 7)
 	_ = client.Database(MongoDBName).
-		CreateCollection(context.TODO(), CollectionGroupInvitation, &options.CreateCollectionOptions{ExpireAfterSeconds: &groupInvitationExpire})
+		CreateCollection(context.TODO(), CollectionOfflineMessage, offlineMessageOptions)
+
+	uniqueOption := options.Index().SetUnique(true)
+	// 创建群邀请表，设置邀请3天过期
+	groupOptions := options.CreateCollection().SetExpireAfterSeconds(Day * 3)
+	_ = client.Database(MongoDBName).
+		CreateCollection(context.TODO(), CollectionGroupInvitation, groupOptions)
+	// 创建群邀请表的唯一索引
+	_, err = client.Database(MongoDBName).
+		Collection(CollectionGroupInvitation).
+		Indexes().
+		CreateOne(context.TODO(), mongo.IndexModel{Keys: bson.A{bson.D{{"userID", 1}, {"groupID", 1}}}, Options: uniqueOption})
+
+	// 创建好友请求表，设置请求3天过期
+	friendOptions := options.CreateCollection().SetExpireAfterSeconds(Day * 3)
+	_ = client.Database(MongoDBName).
+		CreateCollection(context.TODO(), CollectionFriendRequest, friendOptions)
+	// 创建好友请求表的唯一索引
+	_, err = client.Database(MongoDBName).
+		Collection(CollectionFriendRequest).
+		Indexes().
+		CreateOne(context.TODO(), mongo.IndexModel{Keys: bson.A{bson.D{{"requester", 1}, {"target", 1}}}, Options: uniqueOption})
 	return client, nil
 }
