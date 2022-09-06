2ab6f2eb558e15b654357580dde1fa2173bf2623
StellarisJAY <760409558@qq.com>
2022-09-06 15:46:51 +0800

    feat: 同步群聊离线消息RPC接口

--- internal/rpc/msg/service/message.go
+++ internal/rpc/msg/service/message.go
@@ -9,6 +9,7 @@ import (
 	"github.com/stellarisJAY/goim/pkg/proto/pb"
 	"github.com/stellarisJAY/goim/pkg/snowflake"
 	"github.com/stellarisJAY/goim/pkg/wordfilter"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type MessageServiceImpl struct {
@@ -39,12 +40,8 @@ func NewMessageServiceImpl() *MessageServiceImpl {
 func (m *MessageServiceImpl) SyncOfflineMessages(ctx context.Context, request *pb.SyncMsgRequest) (*pb.SyncMsgResponse, error) {
 	var messages []*model.OfflineMessage
 	var err error
-	if *request.Flag == int32(pb.MessageFlag_Group) {
-		messages, err = dao.ListOfflineGroupMessages(request.UserID, *request.From, request.LastSeq)
-	} else {
-		// 查询 to == userID AND seq > lastSeq
-		messages, err = dao.ListOfflineMessages(request.UserID, request.LastSeq)
-	}
+	// 查询 to == userID AND seq > lastSeq
+	messages, err = dao.ListOfflineMessages(request.UserID, request.LastSeq)
 	if err != nil {
 		return &pb.SyncMsgResponse{Code: pb.Error, Message: err.Error()}, nil
 	}
@@ -72,4 +69,49 @@ func (m *MessageServiceImpl) SyncOfflineMessages(ctx context.Context, request *p
 		response.LastSeq = msgs[len(msgs)-1].Seq
 	}
 	return response, nil
+}
+
+// SyncOfflineGroupMessages 同步离线的群聊消息
+// 1. 遍历需要同步的群聊列表。
+// 2. 查询每个群聊的离线消息。
+// 3. 将离线消息从 DO 转换成 DTO
+func (m *MessageServiceImpl) SyncOfflineGroupMessages(ctx context.Context, request *pb.SyncGroupMsgRequest) (*pb.SyncGroupMsgResponse, error) {
+	groups, timestamps := request.Groups, request.Timestamps
+	groupMessages := make([]*pb.SingleGroupMessages, len(groups))
+	for i := 0; i < len(groups) && i < len(timestamps); i++ {
+		groupID, lastTimestamp := groups[i], timestamps[i]
+		messages, err := dao.ListOfflineGroupMessages(request.UserID, groupID, lastTimestamp)
+		if err != nil {
+			if err == mongo.ErrNoDocuments {
+				continue
+			} else {
+				return &pb.SyncGroupMsgResponse{Code: pb.Error, Message: err.Error()}, nil
+			}
+		}
+		groupMessages[i] = &pb.SingleGroupMessages{
+			GroupID:      groupID,
+			StartTimeout: lastTimestamp,
+			LastTimeout:  messages[len(messages)-1].Timestamp,
+			Msgs:         OfflineMessagesToBaseMessages(messages),
+		}
+	}
+	return &pb.SyncGroupMsgResponse{
+		Code:          pb.Success,
+		GroupMessages: groupMessages,
+	}, nil
+}
+
+func OfflineMessagesToBaseMessages(offlineMessages []*model.OfflineMessage) []*pb.BaseMsg {
+	msgs := make([]*pb.BaseMsg, len(offlineMessages))
+	for i, m := range offlineMessages {
+		msgs[i] = &pb.BaseMsg{
+			From:      m.From,
+			To:        m.From,
+			Content:   string(m.Content),
+			Flag:      pb.MessageFlag(m.Flag),
+			Timestamp: m.Timestamp,
+			Id:        m.ID,
+		}
+	}
+	return msgs
 }
