f3c3d0c4736b29254c75885212f8bab84e8de29b
StellarisJAY <760409558@qq.com>
2022-10-13 15:46:19 +0800

    feat: int64列表与string列表转换

--- pkg/stringutil/string.go
+++ pkg/stringutil/string.go
@@ -30,3 +30,23 @@ func HexString(bytes []byte) string {
 func HexStringToInt64(str string) (int64, error) {
 	return strconv.ParseInt(str, 16, 64)
 }
+
+func StringListToInt64(strs []string) []int64 {
+	nums := make([]int64, len(strs))
+	for i, str := range strs {
+		if num, err := strconv.ParseInt(str, 10, 64); err == nil {
+			nums[i] = num
+		} else {
+			return nums[:i]
+		}
+	}
+	return nums
+}
+
+func Int64ListToString(list []int64) []string {
+	strs := make([]string, len(list))
+	for i, num := range list {
+		strs[i] = strconv.FormatInt(num, 10)
+	}
+	return strs
+}
