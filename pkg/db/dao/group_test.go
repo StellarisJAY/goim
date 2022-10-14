c4ce351f1160847a9db0fc97db54f8365304f96d
StellarisJAY <760409558@qq.com>
2022-10-14 11:21:09 +0800

    test: user数据库测试

---
+++ pkg/db/dao/group_test.go
@@ -0,0 +1,23 @@
+package dao
+
+import "testing"
+
+func TestAddGroupMember(t *testing.T) {
+
+}
+
+func TestFindGroupInfo(t *testing.T) {
+
+}
+
+func TestGroupMemberExists(t *testing.T) {
+
+}
+
+func TestListGroupInfos(t *testing.T) {
+
+}
+
+func TestInsertGroup(t *testing.T) {
+
+}
