89bc9a523ae9a09639f8931c6d8a151d1218ae49
StellarisJAY <760409558@qq.com>
2022-11-03 10:50:35 +0800

    feat: 删除原来的好友申请和群邀请表，添加通知表

--- pkg/db/mongo.go
+++ pkg/db/mongo.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"github.com/stellarisJAY/goim/pkg/config"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -30,42 +31,33 @@ func InitMongoDB() (*mongo.Client, error) {
 	defer cancelFunc()
 	err = client.Connect(ctx)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("connect to mongoDB error %w", err))
 	}
 	err = client.Ping(ctx, nil)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("ping mongoDB error %w", err))
 	}
 
-	// 创建离线消息缓存表，离线消息只保留7天
+	// 创建离线消息表，离线消息只保留7天
 	offlineMessageOptions := options.CreateCollection().SetExpireAfterSeconds(Day * 7)
 	_ = client.Database(MongoDBName).
 		CreateCollection(context.TODO(), CollectionOfflineMessage, offlineMessageOptions)
 
 	uniqueOption := options.Index().SetUnique(true)
-	// 创建群邀请表，设置邀请3天过期
-	groupOptions := options.CreateCollection().SetExpireAfterSeconds(Day * 3)
-	_ = client.Database(MongoDBName).
-		CreateCollection(context.TODO(), CollectionGroupInvitation, groupOptions)
-	// 创建群邀请表的唯一索引
-	_, err = client.Database(MongoDBName).
-		Collection(CollectionGroupInvitation).
+	expireOption := options.CreateCollection().SetExpireAfterSeconds(Day * 3)
+	// 创建通知表，通知仅保存三天
+	_ = client.Database(MongoDBName).CreateCollection(context.TODO(), CollectionNotification, expireOption)
+	// 创建通知的唯一索引，避免重复通知（通知类型仅有：好友申请、进群邀请相关的通知，这些通知不能重复出现）
+	_, err = client.Database(MongoDBName).Collection(CollectionNotification).
 		Indexes().
-		CreateOne(context.TODO(), mongo.IndexModel{Keys: bson.D{{"userID", 1}, {"groupID", 1}}, Options: uniqueOption})
+		CreateOne(context.TODO(),
+			mongo.IndexModel{
+				Keys:    bson.D{{"receiver", 1}, {"triggerUser", 1}, {"type", 1}},
+				Options: uniqueOption,
+			})
 	if err != nil {
-		panic("failed to create collection: group_invitations, " + err.Error())
+		panic(fmt.Errorf("create notification unique index error %w", err))
 	}
-	// 创建好友请求表，设置请求3天过期
-	friendOptions := options.CreateCollection().SetExpireAfterSeconds(Day * 3)
-	_ = client.Database(MongoDBName).
-		CreateCollection(context.TODO(), CollectionFriendRequest, friendOptions)
-	// 创建好友请求表的唯一索引
-	_, err = client.Database(MongoDBName).
-		Collection(CollectionFriendRequest).
-		Indexes().
-		CreateOne(context.TODO(), mongo.IndexModel{Keys: bson.D{{"requester", 1}, {"target", 1}}, Options: uniqueOption})
-	if err != nil {
-		panic("failed to create collection: friend_requests, " + err.Error())
-	}
+
 	return client, nil
 }
