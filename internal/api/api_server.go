05031a2802466f8f727ae55d06d0862eec75aac9
StellarisJAY <760409558@qq.com>
2022-11-04 21:17:17 +0800

    feat: api接口注释

--- internal/api/api_server.go
+++ internal/api/api_server.go
@@ -32,17 +32,22 @@ func Init() {
 	{
 		messageParty.Use(middleware.TokenVerifier)
 		messageParty.Done(middleware.ErrorHandler)
+		// 查询离线消息
 		messageParty.Get("/offline/{seq:int64}", handler.SyncOfflineMessageHandler)
+		// 查询群聊离线消息
 		messageParty.Get("/offline/group", handler.SyncOfflineGroupMessages)
+		// 查询最新的群聊消息
 		messageParty.Get("/offline/group/latest", handler.SyncLatestGroupMessages)
 	}
-
+	// 通知API
 	notificationParty := application.Party("/notification")
 	{
 		notificationParty.Use(middleware.TokenVerifier)
 		notificationParty.Done(middleware.ErrorHandler)
-		notificationParty.Get("/list", handler.ListNotifications)
-		notificationParty.Post("/mark/read/{id:int64}", handler.MarkNotificationReadHandler)
+		// 查询所有通知
+		notificationParty.Get("/list/{notRead:bool}", handler.ListNotifications)
+		// 标记已读
+		notificationParty.Post("/markRead/{id:int64}", handler.MarkNotificationReadHandler)
 	}
 	// 用户信息API
 	userParty := application.Party("/user")
@@ -61,7 +66,6 @@ func Init() {
 		groupParty.Get("/{id:int64}", handler.GroupInfoHandler)
 		groupParty.Get("/members/{id:int64}", handler.GroupMemberHandler)
 		groupParty.Post("/{gid:int64}/invite/{uid:int64}", handler.InviteUserHandler)
-		//groupParty.Get("/invitations", handler.ListInvitationsHandler)
 		groupParty.Post("/invitation/{invID:int64}", handler.AcceptInvitationHandler)
 		groupParty.Get("/list", handler.ListJoinedGroupsHandler)
 	}
@@ -72,8 +76,6 @@ func Init() {
 		friendParty.Done(middleware.ErrorHandler)
 		// 添加好友申请
 		friendParty.Post("/application", handler.InsertFriendApplicationHandler)
-		// 列出好友申请
-		//friendParty.Get("/application", handler.ListFriendApplications)
 		// 接受好友申请
 		friendParty.Put("/application", handler.AcceptFriendHandler)
 		// 获取好友信息
