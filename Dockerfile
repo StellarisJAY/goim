52764629232c1656d68a4cc5c121b706f52e54e0
StellarisJAY <760409558@qq.com>
2022-11-11 10:29:53 +0800

    chore: update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,24 @@
-FROM golang
+FROM golang as build
 
+# copy all codes
 COPY ./ ./goim
-
 # set go env
 ENV GO111MODULE=on
 ENV GOPROXY=https://goproxy.cn,direct
 
 WORKDIR ./goim
-
+RUN chmod +x ./script/*.sh
+# run build script
 RUN /bin/sh ./script/build_all.sh
+
+FROM ubuntu
+# set timezone
+RUN ln -fs /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && dpkg-reconfigure -f noninteractive tzdata
+# copy built target binary from build image
+COPY --from=build /goim/script ./goim/script
+COPY --from=build /goim/target ./goim/target
+# volume for config files
+VOLUME "/goim/config"
+WORKDIR ./goim
+
+CMD["./target/api", "--config=config.yaml"]
--- script/build_all.sh
+++ script/build_all.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 echo "building API server..."
-go build -mod=mod -o ./api ./cmd/api
+go build -mod=mod -o ./target/api ./cmd/api
 echo "building Gateway server..."
-go build -mod=mod -o ./gateway ./cmd/gateway
+go build -mod=mod -o ./target/gateway ./cmd/gateway
 echo "building Friend RPC Service..."
-go build -mod=mod -o ./rpc_friend ./cmd/rpc/friend
+go build -mod=mod -o ./target/rpc_friend ./cmd/rpc/friend
 echo "building Group RPC Service..."
-go build -mod=mod -o ./rpc_group ./cmd/rpc/group
+go build -mod=mod -o ./target/rpc_group ./cmd/rpc/group
 echo "building Message RPC Service..."
-go build -mod=mod -o ./rpc_message ./cmd/rpc/message
+go build -mod=mod -o ./target/rpc_message ./cmd/rpc/message
 echo "building User RPC Service..."
-go build -mod=mod -o ./rpc_user ./cmd/rpc/user
+go build -mod=mod -o ./target/rpc_user ./cmd/rpc/user
 echo "building Transfer Service..."
-go build -mod=mod -o ./transfer ./cmd/transfer
+go build -mod=mod -o ./target/transfer ./cmd/transfer
 
 echo "goim build complete"
